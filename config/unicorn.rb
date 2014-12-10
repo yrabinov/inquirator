bd8eba1a743eff971f9ee0d3d1dfafc2c1dc9d40
Aaron Harpole <aharpole@gmail.com>
2014-12-09 23:38:43 -0800

    adding homepage, unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,42 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout Integer(ENV["UNICORN_TIMEOUT"] || 30)
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    Resque.redis = ENV['REDISTOGO_URL']
+    Rails.logger.info('Connected to Redis')
+  end
+end
