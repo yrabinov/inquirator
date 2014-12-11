d02344769e3868c466003417bcd4606ef29b8348
Aaron Harpole <aharpole@gmail.com>
2014-12-11 01:09:07 -0800

    random tweaks that kept me awake at night

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    
+  def after_sign_in_path_for(resource)
+    question_queues_path
+  end
   
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+    
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
